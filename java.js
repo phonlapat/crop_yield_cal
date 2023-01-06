4a9ef23e31f39e185debda8f0ec499249ef9c2f0
phonlapat <106479643+phonlapat@users.noreply.github.com>
2023-01-07 01:50:10 +0700

    Update java.js

--- java.js
+++ java.js
@@ -1,12 +1,27 @@
 function calculateYield() {
-  // Get the values from the form
+  // Get the input values
   var crop = document.getElementById("crop").value;
   var area = document.getElementById("area").value;
   var yield = document.getElementById("yield").value;
-  
-  // Calculate the total yield
-  var totalYield = area * yield;
-  
+
+  // Preprocess the data
+
+  // Scale the numerical features
+  area = scaler.transform(area);
+  yield = scaler.transform(yield);
+
+  // Encode the categorical features
+  crop = encoder.transform(crop);
+
+  // Convert the input values to an array
+  var input = [crop, area, yield];
+
+  // Use the model to make a prediction
+  var prediction = model.predict(input);
+
+  // Convert the prediction back to the original scale
+  prediction = scaler.inverse_transform(prediction);
+
   // Display the result
-  document.getElementById("result").innerHTML = "The total yield for " + crop + " is " + totalYield + " kg.";
+  document.getElementById("result").innerHTML = "Total yield: " + prediction + "kg";
 }
